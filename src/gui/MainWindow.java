f25c2ae787ba91bdd83c1a1d06faabbf2a0b0dd5
H <heshamscorpe55@gmail.com>
2015-05-03 05:43:46 +0200

    Create 'MainWindow' GUI

---
+++ src/gui/MainWindow.java
@@ -0,0 +1,37 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame{
+    /// Attribute(s) ///
+    private Container mainWindowContainer;
+    private EncryptionForm mainWindowEncryptionForm;
+
+
+    /// Constructor(s) ///
+    public MainWindow(String text) {
+        super(text);
+
+        /// initialize attributes ///
+        mainWindowContainer = getContentPane();
+        mainWindowEncryptionForm = new EncryptionForm();
+
+        /// setting properties ////
+        setMaximumSize(new Dimension(500, 500));
+        setMinimumSize(new Dimension(500, 500));
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+    }
+
+    /// Methods(s) ////
+    private void setAttributesBehaviour () {
+
+    }
+    private void setAttributesPositions() {
+
+    }
+}
