ccee3aa08f7511adb87a092b2c3ccfdba7ad7ccd
H <heshamscorpe55@gmail.com>
2015-05-03 06:22:59 +0200

    Create 'AboutWindow'

---
+++ src/gui/AboutWindow.java
@@ -0,0 +1,13 @@
+package gui;
+
+
+import javax.swing.*;
+
+public class AboutWindow extends JFrame {
+
+    /// Constructor ///
+    public AboutWindow(String text) {
+        super(text);
+    }
+
+}
