e16330a86b3bfe49979ec92044f74ee00bb4d4ab
H <heshamscorpe55@gmail.com>
2015-05-05 22:08:31 +0200

    Implements the cryptographical behaviour

---
+++ src/controller/ProgramLogic.java
@@ -0,0 +1,10 @@
+package controller;
+
+import gui.CryptographicInfo;
+
+public class ProgramLogic implements CryptographicalBehaviour{
+    @Override
+    public void EncryptOrDecrypt(CryptographicInfo event) {
+        /// TODO add program logic here (Encryption and Decryption) ///
+    }
+}
